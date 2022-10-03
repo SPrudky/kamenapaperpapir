d04d3d5710e3de0e7cf0f44fc722818ce59720b6
Samuel Prudký <Prudky.Samuel@stud.vassboskovice.cz>
2022-10-03 14:15:00 +0200

    Přidejte soubory projektu.

---
+++ kamenapaperpapir/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using System.Windows.Threading;
+
+namespace kamenapaperpapir
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        player1 _1 = new player1(10);
+        player1 _2 = new player1(10);
+
+        DispatcherTimer timer = new DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 500);
+            timer.Tick += new EventHandler(tah) ;
+        }
+        public void tah(object sender, EventArgs e)
+        {
+            _1.tah();
+            _2.tah();
+
+            switch (_1.last)
+            {
+                case 0:
+                    o1.Source = new BitmapImage(new Uri("Images/R.png", UriKind.Relative));
+                    break;
+                case 1:
+                    o1.Source = new BitmapImage(new Uri("Images/S.png", UriKind.Relative));
+                    break;
+                case 2:
+                    o1.Source = new BitmapImage(new Uri("Images/P.png", UriKind.Relative));
+                    break;
+                    
+
+            }
+            switch (_2.last)
+            {
+                case 0:
+                    o2.Source = new BitmapImage(new Uri("Images/R.png" , UriKind.Relative));
+                    break;
+
+                case 1:
+                    o2.Source = new BitmapImage(new Uri("Images/S.png", UriKind.Relative));
+                    break;
+                case 2:
+                    o2.Source = new BitmapImage(new Uri("Images/P.png", UriKind.Relative));
+                    break;
+
+
+
+            }
+
+
+
+            
+
+
+
+        }
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Start();
+        }
+    }
+}
+   
+
---
+++ kamenapaperpapir/player1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace kamenapaperpapir
+{
+    class player1
+    {
+        public int money { get; set; }
+        public int last { get; set; }
+
+        public player1(int basem)
+        {
+            money = basem;
+        }
+
+        public void tah()
+        {
+            Random r = new Random();
+            last = r.Next(0,3);
+            
+           
+            
+        }
+    }
+    public enum nazev 
+    { 
+        rock,
+        paper,
+        scisors
+    }
+
+
+
+    
+}
